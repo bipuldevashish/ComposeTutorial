a3d4ab67db6c0aafddd91b0355c97cb6166d8be9
Bipul Devashish <bipuldevashish@gmail.com>
2021-02-12 23:59:56 +0530

    navigation components added.

--- app/src/main/java/com/bipuldevashish/melody/MainActivity.kt
+++ app/src/main/java/com/bipuldevashish/melody/MainActivity.kt
@@ -2,10 +2,31 @@ package com.bipuldevashish.melody
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val navView : BottomNavigationView = findViewById(R.id.nav_view)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment, R.id.searchFragment, R.id.playerFragment, R.id.firebaseFragment, R.id.downloadFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
