070abf0dabd4c3c173c97faa921aaea469189325
Bipul Devashish <bipuldevashish@gmail.com>
2022-07-02 16:46:26 +0530

    Card folding.!

--- composetutorial/app/src/main/java/com/bipuldevashish/composetutorial/ui/mainActivity/MainActivity.kt
+++ composetutorial/app/src/main/java/com/bipuldevashish/composetutorial/ui/mainActivity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.bipuldevashish.composetutorial
+package com.bipuldevashish.composetutorial.ui.mainActivity
 
 import android.content.res.Configuration
 import android.os.Bundle
@@ -9,14 +9,18 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.bipuldevashish.composetutorial.R
 import com.bipuldevashish.composetutorial.data.Message
 import com.bipuldevashish.composetutorial.ui.theme.ComposeTutorialTheme
 
@@ -24,14 +28,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello World!")
+            PreviewMessageCard()
         }
     }
 }
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row {
+
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Row(
+        modifier = Modifier.padding(12.dp)
+    ) {
         Image(
             painter = painterResource(id = R.drawable.img),
             contentDescription = "Contact profile picture",
@@ -41,7 +51,7 @@ fun MessageCard(msg: Message) {
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+        Column(modifier = Modifier.padding(bottom = extraPadding)) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
@@ -56,6 +66,9 @@ fun MessageCard(msg: Message) {
                     modifier = Modifier.padding(all = 4.dp),
                     style = MaterialTheme.typography.body2
                 )
+            }
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show Less" else "Show More")
             }
         }
     }
